7ffa499e9e8ecb23875403bcdbebb908df7854bf
dingyiwei <846414640@qq.com>
2017-01-14 01:08:12 +0800

    bug fix

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
  * Created by dingyiwei on 17-1-13.
  */
 $(document).ready(function () {
-    //window.location.href="HistoryCurriculumStandard/index.html";
+    window.location.href="HistoryCurriculumStandard/index.html";
 })
